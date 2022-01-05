8b2eb27aaaf276c9a99dc4595a42ed72ce10fed6
ASHMIT-Anand <97156555+ASHMIT-Anand@users.noreply.github.com>
2022-01-05 14:08:04 +0530

    Delete 2.py

--- 2.py
+++
@@ -1,5 +0,0 @@
-#program 2
-#factorial of a number is the number multiplied by its predecessors till 1
-#so factorial of 10 would ve 10*9*8*&*6*5*4*3*2*1
-
-print('The factorial of 10 is: ',10*9*8*7*6*5*4*3*2*1)
