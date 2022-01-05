7f141c0be212327af3e018a2b679548975e5fd1d
ASHMIT-Anand <97156555+ASHMIT-Anand@users.noreply.github.com>
2022-01-05 14:08:33 +0530

    Delete 4.py

--- 4.py
+++
@@ -1,5 +0,0 @@
-#program 4
-list=[29, 'December', 20.07]
-print('The data type of 1st item is: '+str(type(list[0])))
-print('The data type of 2nd item is: '+str(type(list[1])))
-print('The data type of 3rd item is: '+str(type(list[2])))
