4f0aca0c110c9ce40f7d8dd31854931134f52712
ASHMIT-Anand <97156555+ASHMIT-Anand@users.noreply.github.com>
2022-01-05 14:08:18 +0530

    Delete 21.py

--- 21.py
+++
@@ -1,7 +0,0 @@
-#program 21
-#mileage of a vehicle
-km = input('Distance travelled by vehicle(in km): ')
-petrol=input('Amount of fuel used for trip (in litres): ')
-mileage = float(km)/float(petrol)
-
-print('Mileage of the vehicle is: ', mileage)
